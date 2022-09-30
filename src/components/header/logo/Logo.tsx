aca4456493dfc726474259c9052961fbcf36466b
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-09-30 11:26:12 +0300

    feat: logo is a link now

--- src/components/header/logo/Logo.tsx
+++ src/components/header/logo/Logo.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { NavLink } from 'react-router-dom';
+
 import s from './Logo.module.scss';
 
 import { ReturnComponentType } from 'types';
@@ -7,14 +9,16 @@ import { ReturnComponentType } from 'types';
 export const Logo = (): ReturnComponentType => {
   return (
     <h1 className={s.logo}>
-      <span className={s.logo_item}>F</span>
-      <span className={s.logo_item}>a</span>
-      <span className={s.logo_item}>k</span>
-      <span className={s.logo_item}>e</span>
-      <span className={s.logo_text}>
-        <span className={s.logo_text_header}>BBS</span>
-        <span className={s.logo_text_underline} />
-      </span>
+      <NavLink to="/">
+        <span className={s.logo_item}>F</span>
+        <span className={s.logo_item}>a</span>
+        <span className={s.logo_item}>k</span>
+        <span className={s.logo_item}>e</span>
+        <span className={s.logo_text}>
+          <span className={s.logo_text_header}>BBS</span>
+          <span className={s.logo_text_underline} />
+        </span>
+      </NavLink>
     </h1>
   );
 };
