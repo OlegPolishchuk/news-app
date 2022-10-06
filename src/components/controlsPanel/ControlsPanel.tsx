9ea0511efd4f3785f27e227d12d09dbe04bd7a63
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-10-06 12:58:44 +0300

    feat: add controlsSelect blur functionality

--- src/components/controlsPanel/ControlsPanel.tsx
+++ src/components/controlsPanel/ControlsPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import s from './ControlsPanel.module.scss';
 
@@ -13,51 +13,105 @@ import {
 } from 'globalConstants';
 import { ReturnComponentType } from 'types';
 
-export const ControlsPanel = (): ReturnComponentType => {
+const ID_FOR_CONTROL_SELECT_CATEGORY = 'ControlSelect_Category';
+const ID_FOR_CONTROL_SELECT_REGIONS = 'ControlSelect_Regions';
+const ID_FOR_CONTROL_SELECT_LANGUAGE = 'ControlSelect_Language';
+
+export const ControlsPanel = React.memo((): ReturnComponentType => {
   const [controlsState, setControlsState] = useState({
     category: DEFAULT_CATEGORIES,
     region: DEFAULT_REGION,
     language: DEFAULT_LANGUAGE,
   });
 
-  const handleCategoryChange = (value: string): void => {
-    setControlsState({ ...controlsState, category: value });
-  };
+  const [isCategoryListOpen, setIsCategoryListOpen] = useState(false);
+  const [isRegionsListOpen, setIsRegionsListOpen] = useState(false);
+  const [isLanguageListOpen, setIsLanguageListOpen] = useState(false);
 
-  const handleRegionChange = (value: string): void => {
-    setControlsState({ ...controlsState, region: value });
-  };
+  const handleCategoryChange = useCallback(
+    (value: string): void => {
+      setControlsState(state => ({
+        ...state,
+        category: value === '' ? DEFAULT_CATEGORIES : value,
+      }));
+    },
+    [controlsState.category],
+  );
 
-  const handleLanguageChange = (value: string): void => {
-    setControlsState({ ...controlsState, language: value });
-  };
+  const handleRegionChange = useCallback(
+    (value: string): void => {
+      setControlsState(state => ({
+        ...state,
+        region: value === '' ? DEFAULT_REGION : value,
+      }));
+    },
+    [controlsState.region],
+  );
+
+  const handleLanguageChange = useCallback(
+    (value: string): void => {
+      setControlsState(state => ({
+        ...state,
+        language: value === '' ? DEFAULT_LANGUAGE : value,
+      }));
+    },
+    [controlsState.language],
+  );
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
-
     console.log(controlsState);
   };
 
+  useEffect(() => {
+    const toggleOptionsList = (e: MouseEvent): void => {
+      const element = e.target as HTMLElement;
+
+      if (element.id === ID_FOR_CONTROL_SELECT_CATEGORY) {
+        setIsCategoryListOpen(isOpen => !isOpen);
+      }
+      if (element.id === ID_FOR_CONTROL_SELECT_REGIONS) {
+        setIsRegionsListOpen(isOpen => !isOpen);
+      }
+      if (element.id === ID_FOR_CONTROL_SELECT_LANGUAGE) {
+        setIsLanguageListOpen(isOpen => !isOpen);
+      }
+    };
+
+    window.addEventListener('click', toggleOptionsList);
+
+    return () => window.removeEventListener('click', toggleOptionsList);
+  }, [isCategoryListOpen]);
+
   return (
     <div className={s.controlPanel}>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className={s.controlPanel_form}>
         <ControlSelect
+          label="Category:"
           options={CATEGORIES}
           onChangeOption={handleCategoryChange}
           value={controlsState.category}
+          isOpen={isCategoryListOpen}
+          id={ID_FOR_CONTROL_SELECT_CATEGORY}
         />
         <ControlSelect
+          label="Region:"
           options={REGIONS}
           onChangeOption={handleRegionChange}
           value={controlsState.region}
+          isOpen={isRegionsListOpen}
+          id={ID_FOR_CONTROL_SELECT_REGIONS}
         />
         <ControlSelect
+          label="Language:"
           options={LANGUAGES}
           onChangeOption={handleLanguageChange}
           value={controlsState.language}
+          isOpen={isLanguageListOpen}
+          id={ID_FOR_CONTROL_SELECT_LANGUAGE}
         />
         <button type="submit">Save</button>
       </form>
     </div>
   );
-};
+});
--- src/components/controlsPanel/controlSelect/ControlSelect.tsx
+++ src/components/controlsPanel/controlSelect/ControlSelect.tsx
@@ -1,27 +1,77 @@
 import React, { ChangeEvent } from 'react';
 
+import s from './ControlSelect.module.scss';
+
+import iconArrowDown from 'assets/icons/icon_arrow_down.png';
+import iconArrowUp from 'assets/icons/icon_arrow_up.png';
 import { ReturnComponentType } from 'types';
 
 interface Props {
   options: string[];
   onChangeOption: (value: string) => void;
   value: string;
+  label?: string;
+  isOpen: boolean;
+  id: string;
 }
 
-export const ControlSelect = ({
-  options,
-  onChangeOption,
-  value,
-}: Props): ReturnComponentType => {
-  const handleChange = (event: ChangeEvent<HTMLSelectElement>): void => {
-    onChangeOption(event.currentTarget.value);
-  };
+export const ControlSelect = React.memo(
+  ({ options, onChangeOption, value, label, isOpen, id }: Props): ReturnComponentType => {
+    const arrayOfValues = value.split(' ');
+    const labelTitle =
+      arrayOfValues.length === 1 ? arrayOfValues : `${arrayOfValues[0]}...`;
 
-  return (
-    <select defaultValue={value} onChange={handleChange}>
-      {options.map(option => (
-        <option key={option.toString()}>{option}</option>
-      ))}
-    </select>
-  );
-};
+    const arrowBgStyle = {
+      backgroundImage: `url(${isOpen ? iconArrowUp : iconArrowDown})`,
+    };
+
+    const handleChangeOption = (event: ChangeEvent<HTMLInputElement>): void => {
+      let newCurrentValue;
+
+      if (event.currentTarget.checked) {
+        newCurrentValue = `${event.target.value} ${value}`;
+      } else {
+        newCurrentValue = value
+          .split(' ')
+          .filter(value => value !== event.target.value)
+          .join(' ');
+      }
+
+      onChangeOption(newCurrentValue);
+    };
+
+    const handleShowOption = (): void => {};
+
+    return (
+      <div className={s.select_wrapper} id={id}>
+        <div className={s.select_label_wrapper}>
+          <span className={s.select_label}>{label}</span>
+          <button
+            id={id}
+            className={s.select_currentOption}
+            type="button"
+            style={arrowBgStyle}
+            onClick={handleShowOption}
+          >
+            {labelTitle}
+          </button>
+        </div>
+        <ul className={`${s.select} ${isOpen ? s.select_visible : ''}`}>
+          {options.map(option => (
+            <div key={option.toString()} className={s.select_option}>
+              <label>
+                <input
+                  type="checkbox"
+                  value={option}
+                  onChange={handleChangeOption}
+                  checked={value.includes(option)}
+                />
+                <span>{option}</span>
+              </label>
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
+  },
+);
