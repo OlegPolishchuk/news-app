68575d9a2a214c5647078f72f0498ee1dafb9e65
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-11-04 15:46:10 +0300

    fix: getWeather method's url

--- src/api/weather/index.ts
+++ src/api/weather/index.ts
@@ -4,7 +4,7 @@ import { WeatherResponse } from 'types/responses/weatherResponse';
 export class WeatherAPI {
   static getWeather(city: string): Promise<WeatherResponse> {
     return weatherInstance
-      .get(`http://api.weatherstack.com/current?`, {
+      .get(`https://api.weatherstack.com/current?`, {
         params: {
           query: city,
         },
