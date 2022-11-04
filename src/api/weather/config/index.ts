3cc71f1e763e7a732fbcd9fce992b4c815307a36
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-11-04 14:59:09 +0300

    fix: change http tp https at API urls

--- src/api/weather/config/index.ts
+++ src/api/weather/config/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const weatherInstance = axios.create({
-  baseURL: 'http://api.weatherstack.com/',
+  baseURL: 'https://api.weatherstack.com/',
   params: {
     access_key: process.env.REACT_APP_WEATHER_API_KEY,
   },
